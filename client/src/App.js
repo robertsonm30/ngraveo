e94b3476acd0c9304f47531c16814f65eff91a92
Noah <noahtullysanderson@gmail.com>
2018-05-09 19:19:13 -0400

    React, MongoDb set up,

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from './pages/home';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route path="/home" component={HomePage} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose");
+// Initialize Express
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ngraveo";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {});
+
+app.listen(3001, function() {
+  console.log("App running on port 3000!");
+});
